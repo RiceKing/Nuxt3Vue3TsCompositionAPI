63584bcb1476d75106ae7c9dd552e99e769aee80
Вадим Тюкпеев <1992TVA@gmail.com>
2024-08-17 16:41:42 +0700

    add container aside page

---
+++ components/container/AsidePage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="aside">
+            <slot name="aside" />
+        </div>
+        <div class="main">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.container{
+    display: flex;
+}
+
+.aside {
+    flex-basis: 293px;
+}
+
+.main {
+    flex: 1 1;
+}
+</style>
--- pages/jilfond.vue
+++ pages/jilfond.vue
@@ -1,16 +1,34 @@
 <template>
-    <div>
-        <BaseTypography tag="h1" type="title-16" mode="jilfond-typogaphy">
-            Тестовое задание для jilfond
-        </BaseTypography>
-        <BaseTypography tag="p" type="text-14-400" mode="jilfond-typogaphy">
-            Обычный текст
-        </BaseTypography>
-        <BaseTypography tag="p" type="text-14-600" mode="jilfond-typogaphy">
-            Жирный текст
-        </BaseTypography>
-        <img src="/images/EmptyImage.png" />
-    </div>
+    <ContainerAsidePage>
+        <div>
+            <BaseTypography tag="h1" type="title-16" mode="jilfond-typogaphy">
+                Тестовое задание для jilfond
+            </BaseTypography>
+            <BaseTypography tag="p" type="text-14-400" mode="jilfond-typogaphy">
+                Обычный текст
+            </BaseTypography>
+            <BaseTypography tag="p" type="text-14-600" mode="jilfond-typogaphy">
+                Жирный текст
+            </BaseTypography>
+            <img src="/images/EmptyImage.png" />
+        </div>
+
+        <template #aside>
+            <div>
+                <BaseTypography tag="h1" type="title-16" mode="jilfond-typogaphy">
+                    Тестовое задание для jilfond
+                </BaseTypography>
+                <BaseTypography tag="p" type="text-14-400" mode="jilfond-typogaphy">
+                    Обычный текст
+                </BaseTypography>
+                <BaseTypography tag="p" type="text-14-600" mode="jilfond-typogaphy">
+                    Жирный текст
+                </BaseTypography>
+                <img src="/images/EmptyImage.png" />
+            </div>
+        </template>
+    </ContainerAsidePage>
+   
 </template>
 
 <script setup>
