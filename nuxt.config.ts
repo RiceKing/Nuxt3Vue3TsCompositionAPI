cc7efedb43c57185d5635e72535f59249b0f7217
Вадим Тюкпеев <1992TVA@gmail.com>
2024-08-17 13:20:09 +0700

    add device, sass package. add normalize.css project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+        htmlAttrs: {
+            lang: 'ru',
+        },
+        meta: [
+            {
+                name: 'viewport',
+                content:
+                    'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+            },
+        ],
+    },
+  },
+  
+  modules: [
+    '@nuxtjs/device',
+  ],
+
+  ssr: true,
+  devtools: { enabled: true },
+
+  device: {
+    refreshOnResize: true,
+  },
+
+  css: [
+    '@/assets/styles/normalize.css',
+  ],
+
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
 })
